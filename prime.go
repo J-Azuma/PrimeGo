06c7999a7c410f1196e776cca778f1119e9f62b3
Higashiii <37910197+Higashi-J@users.noreply.github.com>
2018-12-25 14:09:14 +0900

    second commit

--- prime.go
+++ prime.go
@@ -2,32 +2,26 @@ package main
 
 import "fmt"
 
-func main() {
-	var primeTable [100]int
-	primeTable[0] = 2 //素数を格納する配列。0番目の要素を２に初期化
-	primeSize := 1    //格納されている素数の個数
-
-	//奇数列を生成
-	for n := 3; n <= len(primeTable); n += 2 {
-		isPrime := true //ポイント
-		for i := 1; i < primeSize; i++ {
-			p := primeTable[i]
-			if p*p > n {
-				break
-			}
-			if n%p == 0 {
-				isPrime = false
-				break
-			}
+func isPrime(n, primeSize int, primeTable []int) bool {
+	for i := 1; i <primeSize; i ++ {
+		p := primeTable[i]
+		if p * p > n {break}
+		if n % p == 0 {
+			return false
 		}
-		if isPrime {
+	}
+	return true
+}
+
+func main(){
+	primeTable := make([]int, 100)
+	primeTable[0] = 2
+	primeSize := 1
+	for n := 3; n < len(primeTable); n += 2 {
+		if isPrime(n, primeSize, primeTable) {
 			primeTable[primeSize] = n
-			primeSize++
+			primeSize ++
 		}
 	}
-	for i := 0; i < primeSize; i++ {
-		fmt.Print(primeTable[i], " ")
-	}
-	fmt.Print("\n")
-
+	fmt.Println(primeTable[:primeSize])
 }
